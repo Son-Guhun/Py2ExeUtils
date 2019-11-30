5f22eca1f7794c58195e99f5a43944a135be661e
Son Guhun <22954418+Son-Guhun@users.noreply.github.com>
2019-11-30 07:52:20 -0300

    Add docstrings to path.py
    Also fixed issue with lstrip and n=0

--- py2exeUtils/path.py
+++ py2exeUtils/path.py
@@ -17,18 +17,40 @@ def commonpath(l):
     return os.path.sep.join(cp)
 
 def up(path, n=1, lastSeparator=True):
+    """Examples:
+    
+    up('C:/Users/SonGuhun') => 'C:\\Users\\'
+    up('C:/Users/SonGuhun', lastSeparator=False) => 'C:\\Users'
+    up('C:\\Users\\SonGuhun\\Documents', n=2) => 'C:\\Users\\'
+    """
     return os.path.abspath(os.path.join(path ,"/".join(['..' for i in range(n)]))) \
            + (os.path.sep if lastSeparator else '')
 
 rstrrstrip=up
 
 def relative(path, depth=1, lastSeparator=True):
+    """Returns a relative path. Examples:
+    
+    relative('C:\\Users\\SonGuhun\\Documents') => 'Documents\\'
+    relative('C:\\Users\\SonGuhun\\Documents', depth=2 => 'SonGuhun\\Documents\\'
+    """
     return os.path.relpath(path,up(path,depth,False)) \
            + (os.path.sep if lastSeparator else '')
 
 def lstrip(path, n=1, lastSeparator=True):
+    """Examples:
+    
+    lstrip('C:\\Users\\SonGuhun\\Documents', n=3) => 'Documents\\'
+    lstrip('C:\\Users\\SonGuhun\\Documents') => 'Users\\SonGuhun\\Documents\\'
+    """    
     return os.path.relpath(path,up(path,1-n+os.path.abspath(path).count(os.path.sep),False)) \
-           + (os.path.sep if lastSeparator else '')
+           + (os.path.sep if lastSeparator else '') if n>0 else path
     
 def strip(path, left=0, right = 0, lastSeparator=True):
+    """Examples:
+    
+    strip('C:\\Users\\SonGuhun\\Documents') => 'C:\\Users\\SonGuhun\\Documents\\'
+    strip('C:\\Users\\SonGuhun\\Documents', left=2) => 'SonGuhun\\Documents\\'
+    strip('C:\\Users\\SonGuhun\\Documents', left=2, right=1) => 'SonGuhun\\'
+    """   
     return lstrip(up(path, right,False),left,False) + (os.path.sep if lastSeparator else '')
